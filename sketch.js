a62973431aa548e36e739094a37af0b0173b2583
oumou318 <oumoudiallo318@gmail.com>
2017-02-23 13:22:46 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,28 @@
-//Oumou is the Driver
-//James is the Typer
-
+//James is typing
+//Oumou is Driving
+//we ended up putting a lot more effort and it became a person in a hat.
 
 
 function setup() {
-  createCanvas(1000,1000);
+  createCanvas(600,800);
 }
 
 function draw() {
-  background(0);
-  for(x = 0; x<=width; x+=200) {
-    for (var y = 0; y<=height; y+=100) {
-    fill(random(255), random(255),random(255));
-    frameRate(10);
-    rect(x,y,50,50);
-    }
-  }
-  
+  background("green");
+  hat(300,400);
+  hat(150,150);
+  hat(0,0);
+  hat(400,400);
+}
+
+function hat(x,y) {
+  noStroke()
+  fill("black");
+  rect(x+5,y+60,50,50);
+  fill("yellow");
+  ellipse(x+30,y+40,40,40);
+  fill("purple");
+  rect(x+10,y,40,40);
+  rect(x,y+20,60,20);
+
 }

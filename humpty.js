68bc0ebd78cc7f92b168c2e86bad5044b36b214a
Oumou <oumoudiallo318@gmail.com>
2017-02-24 10:50:58 -0500

    Add files via upload

---
+++ humpty.js
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(600,600);
+}
+
+function draw() {
+  background(53,170,227);
+  line(20,148,10,200); // i can't get the lines to show without having stroke LINE FARTHEST TO THE LEFT
+  line(123,80,250,160); // THE CENTERLINE THE LONGEST ONE
+  line(130,10,200,15); // FARTHEST TO THE RIGHT
+  line(135,45,200,55); // SECOND FARTHEST TO THE RIGHT
+  line(50,140,80,220); // SECOND FARTHEST TO THE RIGTH
+  fill('yellow');
+  arc(0,0,300,300,0,HALF_PI);
+  noFill(255);
+  // noStroke(); // with noStroke i couldn't see the line
+  fill('white');
+  //THE CLOUDS
+  ellipse(420,50,200,50);
+  ellipse(250,60,100,50);
+  ellipse(350,80,200,50);
+  ellipse(450,110,250,50);
+  fill('brown');
+  rect(0,300,600,600); // points of the rect are top left corner and bottom right
+  fill(255,255,178);
+  //THE EGG
+  ellipse(300,300,200,250);
+  fill('purple');
+  rect(200,320,200,70);
+  fill('white');
+  ellipse(240,250,30,40);
+  ellipse(360,250,30,40);
+  fill('black')
+  ellipse(245,248,10,10)
+  ellipse(365,250,10,10)
+  arc
+  line(400)
+  
+}

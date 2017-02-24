e8905ace3f366908640d90acfb141236c194d9e3
Oumou <oumoudiallo318@gmail.com>
2017-02-24 10:39:47 -0500

    Add files via upload

---
+++ colors&shapes2.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(1500,900);
+}
+
+function draw() {
+ //We're making a mural of a little girl.
+ background(241,137,255);
+ //The ellipse is acting aas the head.
+ fill(61,37,37);
+ ellipse(700,450,300,400);
+ fill(0,0,0);
+ //This ellipse is acting as an afro.
+ ellipse(700,300,350,350)
+ fill(255,255,255);
+ //These two ellipses were they eyes.
+ ellipse(750,470,10,20);
+ ellipse(640,470,10,20);
+ mousePressed() {
+   clear()
+ }
+ }

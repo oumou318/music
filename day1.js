7608b466e6f07ea71f762dfcd9e80ffa063576d2
Oumou <oumoudiallo318@gmail.com>
2017-02-24 10:42:19 -0500

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(700,400);
+  background(255,255,0);
+  fill(20,20,60,60);
+  strokeWeight(3);// naomi showed us
+  stroke(color('black'));
+  ellipse(350,200,55,55) // it's in the center. 
+  //changing it from 56,56,55,55 
+  ellipse(40,40,40,40); // mad cool i got two cirlces overlapping!! ok i'm a try to move one. so the height and width 
+                        // are in ratio with the size of the canvas.
+  //adding color
+  
+}
+
+function draw() {
+ //i had a second background here and it didn't show up on play
+ //hexacode
+  
+}

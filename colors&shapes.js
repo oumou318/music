5678bde60cc353bbcf73c3cb7bfa67209793d2ff
Oumou <oumoudiallo318@gmail.com>
2017-02-24 10:38:58 -0500

    Add files via upload

---
+++ colors&shapes.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(850,700);
+}
+//We were trying to make a house.There is a doorknob,a window and a placemat.
+function draw() {
+  background(0,0,255);
+  fill(0,165,83);
+  ellipse(250,250,403,403);
+  fill(61,15,30);
+  ellipse(100,550,100,100);
+  fill(255,255,255);
+  rect(600,200,200,200);
+  fill(61,15,30);
+  rect(200,650,300,300);
+//We alternated between the driving and typing. This was a duel project between Oumou and Zahra
+  
+  
+}

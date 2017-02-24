59860175fdac907b84d0790dcf219e045ca9c8f8
Oumou <oumoudiallo318@gmail.com>
2017-02-24 14:15:53 -0500

    Update J+O loop.js

--- J+O loop.js
+++ J+O loop.js
@@ -9,10 +9,10 @@ function setup() {
 
 function draw() {
   background(0);
-  for(x = 0; x<=width; x+=200) {
-    for (var y = 0; y<=height; y+=100) {
+  for(x = 0; x<=width; x+=200) {                      // loop will end at the width
+    for (var y = 0; y<=height; y+=100) {              // loop will end at the height of the canvas
     fill(random(255), random(255),random(255));
-    frameRate(10);
+    frameRate(10);         // how fast it will change
     rect(x,y,50,50);
     }
   }

dc45779ba182a841257077bbcde7af28fbf8183d
Oumou <oumoudiallo318@gmail.com>
2017-02-24 10:44:28 -0500

    Add files via upload

---
+++ rect.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600,600);
+}
+
+function draw() {
+  background(255,0,255);
+  rect(250,250,56,50);
+  rect(100,150,100,50);
+  rect(200,250,50,100);
+  rect(250,300,150,50);
+  rect(305,100,95,200); // hardest so far like width=95 200=height going down
+  rect(200,100,200,30);
+  rect(200,130,105,120);
+  fill('yellow'); //
+  ellipse(300,200,50,50);
+}
